932075c2e273ad33cc803465b7efeadd1f134f4e
Stanculescu Diana <stanculescu.d@yahoo.com>
2022-05-04 20:13:03 +0300

    Home page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Landing.css";
 const Landing = () => {
-	return (
-		<div className="landing">
-			<div className="dark-overlay">
-				<div className="landing-inner">
-					<div className="buttons">
-						<Link to="/register" className="btn btn-primary">
-							Sign Up
-						</Link>
-						<Link to="/login" className="btn btn-primary">
-							Login
-						</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <div className="buttons">
+            <Link to="/register" className="btn">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-primary">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
